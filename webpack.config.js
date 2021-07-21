643cca81c6bbc536499b2c800f9c97531c2b30cb
Erin O'Connor <oconnor.wa@gmail.com>
2021-07-21 14:54:54 -0700

    added webpack and babelrc config files

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  mode: 'development',
+  entry: __dirname + '/client/src/index.jsx',
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '/client/dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+};
