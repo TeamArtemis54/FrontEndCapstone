e162fe8507889c5d47205660f373612cb40e0fea
Jinhoo Bong <jinhoobong@gmail.com>
2021-07-23 23:45:52 -0700

    Set down basic get request for Products API

--- server/index.js
+++ server/index.js
@@ -1,14 +1,46 @@
 // we have to import express
 var express = require('express');
+const axios = require('axios');
+
+// Path
 const path = require('path');
+
 // create a routes file
 // var router = require('./routes.js');
+
+
 var app = express();
 app.use(express.static(__dirname + '/../client/dist'));
 
 // app.get('/', (req, res) => {
 //   res.sendFile(path.join(__dirname, '../client/src', 'index.html'));
 // })
+
+const url = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp';
+
+// create ROUTES FOLDER
+// SEPARATE API'S INTO SEPARATE MODEL/CONTROLLERS??
+// WATCH DATABASES VIDEO TO SEE WHAT THE PROPER WAY IS
+
+
+
+// PRODUCTS API
+app.get('/products', (req, res) => {
+  axios({
+    method: 'get',
+    url: url + '/products',
+    headers: {
+      'Authorization': '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'
+    }
+  })
+    .then((data) => {
+      res.send(data.data);
+    })
+    .catch((err) => {
+      // handle error better
+      console.log(err);
+    })
+})
 
 const port = 3000;
 app.listen(port, () => {
---
+++ server/routes.js
@@ -0,0 +1,12 @@
+var controller = require('./controllers');
+var router = require('express').Router();
+
+// fill this out when you refactor maybe?
+
+// connect controller methods to their corresponding routes
+router.get('');
+router.get('');
+router.get('');
+router.get('');
+
+module.exports = router;
