a84ed4ab5ead12eb83a7803bb66239137bf25d89
Erin O'Connor <oconnor.wa@gmail.com>
2021-07-26 13:06:21 -0700

    added a basic header to hold the space

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import Header from './components/Header/Header.jsx';
+
+// RELATED PRODUCTS
 import RelatedProducts from './components/RelatedProducts/RelatedProducts.jsx';
+
+// RATINGS AND REVIEWS
 import RatingsAndReviews from './components/RatingsAndReviews/RatingsAndReviews.jsx';
 import Ratings from './components/RatingsAndReviews/Ratings/Ratings.jsx';
 import Reviews from './components/RatingsAndReviews/Reviews/Reviews.jsx';
+
+// OVERVIEW
 
 class App extends React.Component {
   constructor() {
@@ -13,6 +21,7 @@ class App extends React.Component {
   render () {
     return (
       <div>
+        <Header />
         <h1>Team Artemis</h1>
         <RelatedProducts />
 
