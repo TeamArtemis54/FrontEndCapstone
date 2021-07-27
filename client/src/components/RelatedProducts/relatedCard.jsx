e4cbc9a3704466c417ccd82d0ed019ff38c1ebf7
Jinhoo Bong <jinhoobong@gmail.com>
2021-07-27 15:29:04 -0700

    Setting up use of endpoints and boilerplate

--- client/src/components/RelatedProducts/relatedCard.jsx
+++ client/src/components/RelatedProducts/relatedCard.jsx
@@ -1,15 +1,41 @@
 import React from 'react';
+const axios = require('axios');
 
 let RelatedCard = (props) => {
+
+  function getProductInfo() {
+    axios.get(`/api/products/${props.productId}`)
+      .then((response) => {
+        console.log(response);
+      });
+  }
+
   return (
+    getProductInfo(),
     <div className="card">
-      <img className="cardImage" src=""></img>
-      <p>CATEGORY</p>
-      <p>Expanded Product Name with Extra Text</p>
-      <p>$123</p>
+      {/* <img className="cardImage" src=""></img>
+      <p>{props.category}</p>
+      <p>{props.name}</p>
+      <p>{props.price}</p> */}
       {/* Star Component */}
+      <p>{props.product}</p>
     </div>
   )
 }
+
+// class RelatedCard extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+
+//   render(props) {
+//     return (
+//       <div>
+//         <p>{this.props.product}</p>
+//       </div>
+//     )
+//   }
+
+// }
 
 export default RelatedCard;
--- client/src/components/RelatedProducts/relatedProducts.jsx
+++ client/src/components/RelatedProducts/relatedProducts.jsx
@@ -1,14 +1,52 @@
 import React from 'react';
 import RelatedCard from './relatedCard.jsx';
+import './relatedStyles.css';
+const axios = require('axios');
 
 // This component will hold the first list which will be the related products that were determined internally
 
-let RelatedProducts = () => {
-  return (
-    <div>
-      <RelatedCard />
-    </div>
-  )
-}
+// related API products returns an array of the related items ids
+// given the array of ids,
+// we can generate each product information by using /products/:product_id
 
+// will use default value item as first item 17067
+
+// images will require 'products/productId/styles --> data.results.photos.thumbnail_url
+
+class RelatedProducts extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentProductId: 17067,
+      relatedItems: []
+    }
+  }
+
+  getRelatedItems() {
+    axios.get(`http://localhost:3000/api/products/${this.state.currentProductId}/related`)
+      .then((response) => {
+        let related = response.data;
+        this.setState({
+          relatedItems: related
+        })
+      })
+  }
+
+  componentDidMount() {
+    this.getRelatedItems();
+  }
+
+  render() {
+    return (
+      <div className="RelatedBox">
+        <h3 className="relatedTitle">Related Products</h3>
+        {this.state.relatedItems.map((item, i) => {
+          return <RelatedCard product={item} key={i}/>
+        })}
+      </div>
+    )
+  }
+
+}
 export default RelatedProducts;
