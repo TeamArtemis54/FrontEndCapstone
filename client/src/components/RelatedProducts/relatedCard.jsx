14e51eb03cdd4294a2b4bd1a03061efe0d66ed1c
Jinhoo Bong <jinhoobong@gmail.com>
2021-07-31 16:33:27 -0700

    Updated widget to be more responsive. Took Erin's advice on adding box shadow. Outlined containers for visual understanding.

--- client/src/components/RelatedProducts/relatedCard.jsx
+++ client/src/components/RelatedProducts/relatedCard.jsx
@@ -3,16 +3,22 @@ import React from 'react';
 let RelatedCard = (props) => {
   return (
     <div className="relatedCard" onClick={(e) => props.clickFn(props.product, e)}>
-      <div className="card-imgContainer">
+      <div className="card_img">
         {props.thumbnails[0] === null ?
-        <div className="card-thumbnail">No image available</div> :
-        <img className="card-thumbnail" src={props.thumbnails}/> }
+        <div className="card_img__thumbnail">No image available</div> :
+        // <img className="card-thumbnail" src={props.thumbnails}/>
+        <div className="card_img__thumbnail" style={{
+          backgroundImage: `url(${props.thumbnails})`
+        }}>
+          <p className="card_img__star">&#9733;</p>
+        </div>
+        }
       </div>
       {/* maybe send message that says no image available */}
-      <div className="card-infoContainer">
-        <p className="relatedItem-category">{props.product.category}</p>
-        <p className="relatedItem-name">{props.product.name}</p>
-        <p className="relatedItem-price">${props.product.default_price}</p>
+      <div className="card_info">
+        <p className="card_info__category">{props.product.category}</p>
+        <p className="card_info__name">{props.product.name}</p>
+        <p className="card_info__price">${props.product.default_price}</p>
         <p>stars component</p>
       </div>
     </div>
--- client/src/components/RelatedProducts/relatedProductsWidget.jsx
+++ client/src/components/RelatedProducts/relatedProductsWidget.jsx
@@ -134,9 +134,9 @@ class RelatedProductsWidget extends React.Component {
 
   render() {
     return (
-      <div className="widget-container">
-        <h3 className="widget-title">Related Products</h3>
-        <div className="relatedlist-container">
+      <div className="widget_container">
+        <h3 className="widget_title">Related Products</h3>
+        <div className="relatedlist_container">
           {this.state.relatedItems.map((item, i) => {
             return <RelatedList clickFn={this.handleClick} productId={item} key={i} />
           })}
@@ -148,3 +148,83 @@ class RelatedProductsWidget extends React.Component {
 
 }
 export default RelatedProductsWidget;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useState, useEffect } from 'react';
+// import RelatedList from './relatedList.jsx';
+// import CompareModal from './comparisonModal.jsx';
+// const axios = require('axios');
+
+// // This component will hold the first list which will be the related products that were determined internally
+
+// // related API products returns an array of the related items ids
+// // given the array of ids,
+// // we can generate each product information by using /products/:product_id
+
+// // will use default value item as first item 17067
+
+// // images will require 'products/productId/styles --> data.results.photos.thumbnail_url
+
+// const RelatedProductsWidget = (props) => {
+
+//   const {product} = props;
+
+//   // this will store the current product that the page is on
+//   const [currentProduct, setCurrentProduct] = useState({});
+
+//   // this will store the related items to the current product
+//   const [relatedItems, setRelatedItems] = useState([]);
+
+//   // this will store whether modal is displayed or not
+//   const [showModal, setShowModal] = useState(false);
+
+//   // this will store clicked product data
+//   const [clickedProductData, setClickedProductData] = useState({});
+
+//   // console.log('product being passed in', product);
+
+//   // this will set
+//   useEffect(() => {
+
+//     setCurrentProduct(product);
+//     // console.log('logged');
+//   }, [product]);
+
+//   useEffect(() => {
+//     // console.log('cur', currentProduct);
+//     getRelatedItems();
+//   }, [currentProduct.id])
+
+//   function getRelatedItems() {
+//     axios.get(`/api/products/${currentProduct.id}/related`)
+//       .then((response) => {
+//         let relatedList = response.data;
+//         // console.log('data', response.data);
+//         setRelatedItems(relatedList);
+//       })
+//       .catch((err) => console.log(err));
+//   }
+
+//   return(
+//     <div className="widget-container">
+//       <h1>HI</h1>
+//       <RelatedList related={relatedItems}/>
+//     </div>
+//   )
+// }
+
+// export default RelatedProductsWidget;
