3d84fd528eb7ff640d41c4b4d00c90dfa9530bb9
Jinhoo Bong <jinhoobong@gmail.com>
2021-08-02 11:26:23 -0700

    utilized stars component into my card component with correct data pulled

--- client/src/components/RelatedProducts/relatedCard.jsx
+++ client/src/components/RelatedProducts/relatedCard.jsx
@@ -1,28 +1,60 @@
 import React from 'react';
+import Stars from '../reusable_components/Stars.jsx';
 
-let RelatedCard = (props) => {
-  return (
-    <div className="relatedCard" onClick={(e) => props.clickFn(props.product, e)}>
-      <div className="card_img">
-        {props.thumbnails[0] === null ?
-        <div className="card_img__thumbnail">No image available</div> :
-        // <img className="card-thumbnail" src={props.thumbnails}/>
-        <div className="card_img__thumbnail" style={{
-          backgroundImage: `url(${props.thumbnails})`
-        }}>
-          <p onClick={(e) => props.favorite(e)} className="card_img__star">&#9733;</p>
+const axios = require('axios');
+
+class RelatedCard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      reviewsMeta: {}
+    }
+
+    this.getReviewsMeta = this.getReviewsMeta.bind(this);
+  }
+
+  getReviewsMeta() {
+    console.log('id', this.props.product.id);
+    axios.get(`/api/reviews/meta/${this.props.product.id}`)
+      .then((response) => {
+        console.log(response.data);
+        this.setState({
+          reviewsMeta: response.data
+        })
+      })
+      .catch((err) => console.log(err));
+  }
+
+  componentDidMount() {
+    this.getReviewsMeta();
+  }
+
+  render() {
+    console.log('reviewsmeta', this.state.reviewsMeta);
+    return (
+      <div className="relatedCard" onClick={(e) => this.props.clickFn(this.props.product, e)}>
+        <div className="card_img">
+          {this.props.thumbnails[0] === null ?
+          <div className="card_img__thumbnail">No image available</div> :
+          // <img className="card-thumbnail" src={props.thumbnails}/>
+          <div className="card_img__thumbnail" style={{
+            backgroundImage: `url(${this.props.thumbnails})`
+          }}>
+            <p onClick={(e) => this.props.favorite(e)} className="card_img__star">&#9733;</p>
+          </div>
+          }
         </div>
-        }
+        {/* maybe send message that says no image available */}
+        <div className="card_info">
+          <p className="card_info__category">{this.props.product.category}</p>
+          <p className="card_info__name">{this.props.product.name}</p>
+          <p className="card_info__price">${this.props.product.default_price}</p>
+          <Stars review_meta = {this.state.reviewsMeta}/>
+        </div>
       </div>
-      {/* maybe send message that says no image available */}
-      <div className="card_info">
-        <p className="card_info__category">{props.product.category}</p>
-        <p className="card_info__name">{props.product.name}</p>
-        <p className="card_info__price">${props.product.default_price}</p>
-        <p>stars component</p>
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default RelatedCard;
