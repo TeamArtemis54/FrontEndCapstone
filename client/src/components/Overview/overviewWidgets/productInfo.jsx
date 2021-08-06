d6dc5eda6bcd56f132c6c15c6997e34558057b40
Zach <zachary.bretz@gmail.com>
2021-08-06 12:27:04 -0700

    Added Stars component to productInfo

--- client/src/components/Overview/overviewWidgets/productInfo.jsx
+++ client/src/components/Overview/overviewWidgets/productInfo.jsx
@@ -1,11 +1,34 @@
 import React, {Component} from 'react';
 import ReactDom from 'react-dom';
+import Stars from './../../reusable_components/Stars.jsx';
+const axios = require('axios');
+
 
 
 class ProductInfo extends Component{
   constructor(props){
     super(props)
+
+    this.state = {
+      reviewsMeta: {}
+    }
   }
+
+  getReviewsMeta() {
+    // axios.get(`/api/reviews/meta/${this.props.product.id}`)
+    axios.get(`/api/reviews/meta/1701`)
+      .then((response) => {
+        this.setState({
+          reviewsMeta: response.data
+        })
+      })
+      .catch((err) => console.log(err));
+  }
+
+  componentDidMount() {
+    this.getReviewsMeta();
+  }
+
   render(){
 
     return(
@@ -13,6 +36,8 @@ class ProductInfo extends Component{
       <>
 
       <div>
+
+        <Stars review_meta={this.state.reviewsMeta} />
 
         <p id='category-name'>{this.props.productCategory}</p>
         <p id='product-name'>{this.props.productName}</p>
